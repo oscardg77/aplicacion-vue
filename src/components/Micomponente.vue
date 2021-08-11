852d9f181712a0042489bfe18952a3e99e681560
oscardg77 <82647316+oscardg77@users.noreply.github.com>
2021-08-11 21:23:52 +0200

    aplicacion-vue

---
+++ src/components/Micomponente.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hola que jase</h1>
+        <h2>{{ texto }}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'micomponente',
+    data() {
+        return {
+            texto: "soy el ma mejo"
+        }
+    }
+}
+</script>
+
+
+
+
