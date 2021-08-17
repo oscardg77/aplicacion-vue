6884f6aae742460b753c527bbdc98c2577f99bab
oscardg77 <82647316+oscardg77@users.noreply.github.com>
2021-08-17 12:58:36 +0200

    subir aplicacion terminada

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <micomponente> </micomponente>
+  <div>
+    <div id="header">
+    <Search v-on:query-change="querySearch" />
+    </div>
+    <div id="main-container">
+    <h2>Todos</h2>
+    <TodosAdd v-on:add-todo="addTodo" />
+    <Todos v-bind:todosList="copyTodos" v-on:delete-todo="deleteTodo" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Micomponente from './components/Micomponente.vue';
+import Search from './components/Search';
+import Todos from './components/Todos';
+import TodosAdd from './components/TodosAdd';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Micomponente
-  }
+     Search,
+     Todos,
+     TodosAdd
+    
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id );
+      this.copyTodos = [... this.todos];
+    },
+      addTodo(){
+        this.todos.push();
+        this.copyTodos = [... this.todos];
+      },
+      querySearch(query){
+        if(query.trim() === ''){
+           this.copyTodos = [... this.todos];
+        }else{
+          const temp = this.todos.filter(todo => {
+           return todo.title.includes(query)
+          });
+
+          this.copyTodos = [...temp];
+        }
+      }
+
+  },
+  data(){ 
+    return {
+      todos: [
+        {
+          id: 0,
+          title: 'comprar la cena',
+          completed: false
+        },
+           {
+          id: 1,
+          title: 'fregar los platos',
+          completed: true
+        },
+           {
+          id: 2,
+          title: 'ver la television',
+          completed: false
+        },
+         {
+           id: 3,
+           title: 'terminar tutorial',
+           completed: true
+         }
+      ],
+      copyTodos: []
+    }
+  },
+  created() {   
+   this.copyTodos = [... this.todos];
 }
+  }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}*/
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+  padding: 0;
+  margin: 0;
+}
+#main-container {
+  border: solid 1px #ccc;
+  width: 600px;
+  margin: 100px auto;
+}
+#header {
+  background: #ccc;
+}
+h2 {
+  padding: 0 10px;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todosList">
+        <TodoItem v-bind:todo="todo" v-on:delete-todo="$emit('delete-todo', todo.id)" />
+
+        </div>   
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+  export default {
+    name: 'Todos',
+    props: ['todosList'],
+    components: {TodoItem}
+}
+</script>
+
+<style></style>
